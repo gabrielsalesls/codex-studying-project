0aa751f153d62fc9dd6d97fb8c7a9100655e6f39
gabrielsalesls <salesgabriel311@gmail.com>
2026-05-08 15:37:29 -0300

    feat: initialize wallet service with Spring Boot and add essential configurations

---
+++ Projeto/wallet-service/src/test/java/dev/gabrielsales/wallet_service/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielsales.wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
